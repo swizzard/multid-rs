c4446fe1fa1c723117f96360123f5d63cdef8cb3
Sam Raker <sam.raker@gmail.com>
2025-01-25 17:04:52 -0500

    remove unused error variant, add V2.neighbors_of

--- src/errors.rs
+++ src/errors.rs
@@ -4,6 +4,4 @@ use thiserror::Error;
 pub enum VError {
     #[error("Size mismatch error: expected {expected:?}, got {actual:?}")]
     SizingError { expected: usize, actual: usize },
-    #[error("Error: {field_name:?} out of range")]
-    OutOfRangeError { field_name: &'static str },
 }
--- src/v.rs
+++ src/v.rs
@@ -32,6 +32,9 @@ impl<T, const N_ROWS: usize, const N_COLS: usize> V2<T, N_ROWS, N_COLS> {
     pub fn cols(&self) -> V2Cols<'_, T, N_ROWS, N_COLS> {
         V2Cols::new(&self.data)
     }
+    pub fn neighbors_of(&self, ix: Ix2) -> V2Neighbors<'_, T, N_ROWS, N_COLS> {
+        V2Neighbors::new(&self.data, ix)
+    }
     fn convert_ix(&self, col_ix: usize, row_ix: usize) -> usize {
         row_ix * N_COLS + col_ix
     }
@@ -217,4 +220,135 @@ fn test_v2_cols() {
     let expected: Vec<Vec<&u8>> = vec![vec![&0, &3, &6], vec![&1, &4, &7], vec![&2, &5, &8]];
     let actual = cols.into_iter().collect::<Vec<Vec<&u8>>>();
     assert_eq!(expected, actual);
+}
+
+pub struct V2Neighbors<'a, T, const N_ROWS: usize, const N_COLS: usize> {
+    data: &'a [T],
+    center_col_ix: usize,
+    center_row_ix: usize,
+    curr_col_ix: usize,
+    curr_row_ix: usize,
+}
+
+impl<'a, T, const N_ROWS: usize, const N_COLS: usize> V2Neighbors<'a, T, N_ROWS, N_COLS> {
+    fn new(data: &'a [T], Ix2 { row_ix, col_ix }: Ix2) -> Self {
+        Self {
+            data,
+            center_col_ix: col_ix,
+            center_row_ix: row_ix,
+            curr_col_ix: 0,
+            curr_row_ix: 0,
+        }
+    }
+    fn dec_col(&self) -> Option<usize> {
+        if self.center_col_ix == 0 {
+            None
+        } else {
+            Some(self.center_col_ix - 1)
+        }
+    }
+    fn inc_col(&self) -> Option<usize> {
+        if self.center_col_ix == N_COLS - 1 {
+            None
+        } else {
+            Some(self.center_col_ix + 1)
+        }
+    }
+    fn dec_row(&self) -> Option<usize> {
+        if self.center_row_ix == 0 {
+            None
+        } else {
+            Some(self.center_row_ix - 1)
+        }
+    }
+    fn inc_row(&self) -> Option<usize> {
+        if self.center_row_ix == N_ROWS - 1 {
+            None
+        } else {
+            Some(self.center_row_ix + 1)
+        }
+    }
+}
+
+impl<'a, T, const N_ROWS: usize, const N_COLS: usize> Iterator
+    for V2Neighbors<'a, T, N_ROWS, N_COLS>
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.curr_row_ix < 3 {
+            if self.curr_col_ix == 1 && self.curr_row_ix == 1 {
+                self.curr_col_ix = 2;
+                continue;
+            }
+            let col_ix = match self.curr_col_ix {
+                0 => self.dec_col(),
+                1 => Some(self.center_col_ix),
+                2 => self.inc_col(),
+                _ => panic!("unreachable col"),
+            };
+            let row_ix = match self.curr_row_ix {
+                0 => self.dec_row(),
+                1 => Some(self.center_row_ix),
+                2 => self.inc_row(),
+                _ => panic!("unreachable row"),
+            };
+            if col_ix.is_none() {
+                self.curr_col_ix = if self.curr_col_ix == 2 {
+                    0
+                } else {
+                    self.curr_col_ix + 1
+                };
+                continue;
+            };
+            if row_ix.is_none() {
+                self.curr_row_ix += 1;
+                continue;
+            }
+            if self.curr_col_ix == 2 {
+                self.curr_row_ix += 1;
+                self.curr_col_ix = 0;
+            } else {
+                self.curr_col_ix += 1;
+            }
+            return Some(&self.data[row_ix.unwrap() * N_COLS + col_ix.unwrap()]);
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_neighbors() {
+    let v2: V2<u8, 3, 3> = V2::new((0..=8).collect::<Vec<u8>>()).unwrap();
+
+    let expected_upper_left: Vec<&u8> = vec![&1, &3, &4];
+    let actual_upper_left: Vec<&u8> = v2
+        .neighbors_of(Ix2 {
+            row_ix: 0,
+            col_ix: 0,
+        })
+        .collect();
+    assert_eq!(expected_upper_left, actual_upper_left, "upper_left");
+
+    let expected_center: Vec<&u8> = vec![&0, &1, &2, &3, &5, &6, &7, &8];
+    let actual_center: Vec<&u8> = v2
+        .neighbors_of(Ix2 {
+            row_ix: 1,
+            col_ix: 1,
+        })
+        .collect();
+    assert_eq!(expected_center, actual_center, "center");
+
+    let expected_bottom_middle: Vec<&u8> = vec![&3, &4, &5, &6, &8];
+    let actual_bottom_middle: Vec<&u8> = v2
+        .neighbors_of(Ix2 {
+            row_ix: 2,
+            col_ix: 1,
+        })
+        .collect();
+    assert_eq!(
+        expected_bottom_middle, actual_bottom_middle,
+        "bottom middle"
+    );
 }
